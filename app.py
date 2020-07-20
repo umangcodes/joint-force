64f78a72dd44a6f61a5f51a280b9ea8698014a9f
Umang A <umanga.eng@gmail.com>
2020-07-20 16:49:55 -0400

    calculate_shift_type CORRECT IMPLEMENTATION. add_employee function added.

--- app.py
+++ app.py
@@ -16,27 +16,32 @@ Description: To be updated. See Scope
 
 6. remark/s
 
-7. 
+7. break_time_dictonary 
+
+[{"break_type": "", "break_start_time": , "break_end_time": , "actual_break_start_time": ,"actual_break_end_time": },
+                           "break_type": "", "break_start_time": , "break_end_time": , "actual_break_start_time": ,"actual_break_end_time": },
+                           "break_type": "", "break_start_time": , "break_end_time": , "actual_break_start_time": ,"actual_break_end_time": }]
 """
-
-
+#employee_list = []
 class Employee():
     def __init__(self,name,start_time,end_time,location):
         self.employee_name = name
         self.start_time = start_time
         self.end_time = end_time
         self.location = location
+        self.break_time = []
         self.shift_type = ""
     def __repr__(self):
         return f"Employee {self.employee_name} is working for {self.shift_type} today from {self.start_time} till {self.end_time} at {self.location}"
     def calculate_shift_type(self):
         time_difference = self.end_time - self.start_time
         if time_difference > 4.30:
-            return f"full time"#self.shift_type = "full time"
+            self.shift_type = "full time"
         else:
-            return f"half time"#self.shift_type = "half time"
-
+            self.shift_type = "half time"
 def menu_for_admin():
+
+
     """
     give options to the admin
     options:
@@ -49,3 +54,23 @@ def menu_for_admin():
     - change employee location/role
     :return:
     """
+def add_employee():
+    add_employee = input("enter employee name: ")
+    start_time = int(input("please enter start time: "))
+    end_time = int(input("please enter end time: "))
+    duty = input("please enter employee duty: ")
+    new_employee = add_employee.title()
+    new_employee = Employee(new_employee,start_time,end_time,duty)
+    new_employee.calculate_shift_type()
+ #   employee_list.append(new_employee)
+
+while(1):
+    add_employee()
+    find_employee = input("please enter employee you are looking for:")
+    for element in employee_list:
+        if find_employee.title() == element.employee_name.title():
+            print(element)
+            break
+    else:
+        print("employee not found")
+
