3dcc05f475cacb62b51e0abb32ba3c4c28fab8b1
Umang A <umanga.eng@gmail.com>
2020-07-20 21:20:57 -0400

    Error handling feature added

--- app.py
+++ app.py
@@ -31,36 +31,45 @@ import main_menu
 
 while(1):
     print("Welcome to employee management system!")
-    user_option = int(input("""
-    Please input from the following options:
-    1. add employee
-    2. remove employee
-    3. change employee attributes
-    4. find employee
-    5. display information
-    6. day end(Admin rights required)
-    ==> """))
-    if user_option == 1:
-        main_menu.add_employee()
-    elif user_option == 2:
-        main_menu.remove_employee()
-    elif user_option == 3:
+    try:
         user_option = int(input("""
-        To change start time press 1
-        To change end time press 2
-        To change role press 3
-        """))
-        main_menu.change_employee_attributes(user_option)
-    elif user_option == 4:
-        user_option = int(input("""
-                To find by name press 1
-                To find by start time press 2
-                To find by end time press 3
-                """))
-        main_menu.find_employee(user_option)
-    elif user_option == 5:
-        main_menu.display_employees()
-    elif user_option == 6:
-        main_menu.day_end()
+Please input from the following options:
+1. add employee
+2. remove employee
+3. change employee attributes
+4. find employee
+5. display information
+6. day end(Admin rights required)
+==> """))
+        if user_option == 1:
+            main_menu.add_employee()
+        elif user_option == 2:
+            main_menu.remove_employee()
+        elif user_option == 3:
+            try:
+                user_option = int(input("""
+To change start time press 1
+To change end time press 2
+To change role press 3
+"""))
+                main_menu.change_employee_attributes(user_option)
+            except:
+                print("Invalid")
+        elif user_option == 4:
+            try:
+                user_option = int(input("""
+To find by name press 1
+To find by start time press 2
+To find by end time press 3
+                    """))
+                main_menu.find_employee(user_option)
+            except:
+                print("invalid")
+        elif user_option == 5:
+            main_menu.display_employees()
+        elif user_option == 6:
+            main_menu.day_end()
+    except:
+        print("Invalid command. Please enter correct type of data.")
 
 
