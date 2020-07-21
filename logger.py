34978b83f208d30b913fb84f986c5f94a7a46281
Umang A <umanga.eng@gmail.com>
2020-07-21 14:21:55 -0400

    File: logger created

---
+++ logger.py
@@ -0,0 +1,9 @@
+"""
+Project: Individual project but based on employee management system(Joint Force)
+
+Author: Umang A
+Description: This file generates a log from the available data and creates a csv format file.
+
+In this case, available data is created by day_end function
+
+"""
