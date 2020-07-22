2f5ffd98a734f90b623513208ec0fd8d6ab5859d
Umang A <umanga.eng@gmail.com>
2020-07-22 11:10:34 -0400

    New file containing greeting messages created.

--- app.py
+++ app.py
@@ -26,34 +26,10 @@ Description: To be updated. See Scope
 """
 import main_menu
 import os
-#def menu_for_admin():
+import greeting_messages
 
-attribute_change = """
-To change start time press 1
-To change end time press 2
-To change role press 3
-==> """
 
-find_employee = """
-To find by name press 1
-To find by start time press 2
-To find by end time press 3
-==> """
 
-greeting_message = """
-Please input from the following options:
-1. add employee
-2. remove employee
-3. change employee attributes
-4. find employee
-5. display information
-6. day end(Admin rights required)
-==> """
-
-punch_options = """
-Press 1 to Punch in
-Press 2 to Punch out
-"""
 while(1):
 
 
