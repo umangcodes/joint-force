e2dec32d2e72c111a065c7982880597ad560436d
Umang A <umanga.eng@gmail.com>
2020-07-26 23:08:32 -0400

    V1.01 check change log file

---
+++ global_logic_flags.py
@@ -0,0 +1,9 @@
+"""
+How this file will work?
+
+This file contains all the global variables required for the logical operations.
+"""
+
+class Global_logic_variables:
+    def __init__(self):
+        self.
