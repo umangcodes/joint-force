fbc5259de753bcfa3c7b0f8ced98d37df1b86fd4
Umang A <umanga.eng@gmail.com>
2020-07-28 23:15:20 -0400

    global_logic_flags.py now contain a class where the values for shift types are stored. These are accessed in employee_object.py

--- global_logic_flags.py
+++ global_logic_flags.py
@@ -6,4 +6,10 @@ This file contains all the global variables required for the logical operations.
 
 class Global_logic_variables:
     def __init__(self):
-        self.
+        pass
+
+class Regulations_variables:
+    max_shift_time = 10
+    full_shift_time_difference = 8
+    half_shift_time_difference = 5
+    min_shift_time = 4
