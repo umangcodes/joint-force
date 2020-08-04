26d039b84bb477cc495a123fab413fb5055b92d1
Umang A <umanga.eng@gmail.com>
2020-08-04 00:08:42 -0400

    successful extraction from timedelta and timestamp objects

--- extract_time_attributes.py
+++ extract_time_attributes.py
@@ -1,4 +1,7 @@
 import global_logic_flags
+import datetime
+
+
 def extract_attributes_from_timestamp(input_timestamp):
     """
     This function returns two values. The input to the function is timestamp
@@ -7,8 +10,9 @@ def extract_attributes_from_timestamp(input_timestamp):
     value 2 = min
     """
     output_hour = input_timestamp.hour
-    output_min = input_timestamp.min
-    return output_hour,output_min
+    output_min = input_timestamp.minute
+    return output_hour, output_min
+
 
 def extract_attributes_from_timedelta(input_seconds):
     """
@@ -19,9 +23,10 @@ def extract_attributes_from_timedelta(input_seconds):
     """
     output_hour = input_seconds // 3600
     output_min = input_seconds // 60 - (output_hour * 60)
-    return output_hour,output_min
+    return output_hour, output_min
 
-def extract_time_attributes(extraction_method,input_value):
+
+def extract_time_with_attributes(extraction_method, input_value):
     """
 
     :param extraction_method:   1 for extraction from timestamp
@@ -36,13 +41,23 @@ def extract_time_attributes(extraction_method,input_value):
         if extraction_method == 1:
             if global_logic_flags.system_debug == 1:
                 print("Extracting time attributes using timestamp method")
-            extract_attributes_from_timestamp(input_value)
+            output = extract_attributes_from_timestamp(input_value)
+            return output[0],output[1]
         elif extraction_method == 2:
             if global_logic_flags.system_debug == 1:
                 print("Extracting time attributes using timedelta method")
-            extract_attributes_from_timedelta(input_value)
+            output = extract_attributes_from_timedelta(input_value)
+            return output[0], output[1]
         else:
             print(f"wrong method selected ==> {extraction_method}")
     except:
         print("Something went wrong with the extraction process.")
 
+"""
+time_now = datetime.datetime.now()
+
+b = extract_attributes_from_timestamp(time_now)
+print(b)
+print(b[0])
+print(b[1])
+"""
