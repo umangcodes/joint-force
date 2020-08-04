1a427a7d5166581539995bb11cb5af6f11459042
Umang A <umanga.eng@gmail.com>
2020-08-04 00:10:11 -0400

    formatting changed

--- input_time_converter.py
+++ input_time_converter.py
@@ -11,11 +11,15 @@ input the values in the datetime.time()
 """
 import datetime
 import greeting_messages
+
+
 class Format_time():
     def __init__(self):
         pass
+
     def __repr__(self):
         return f"convert user time input into time or timedelta object"
+
     @staticmethod
     def convert_to_time():
         try:
@@ -27,20 +31,26 @@ class Format_time():
             print(f"Please input appropriate values.")
 
     @staticmethod
-    def convert_to_timedelta():
-        try:
-            operation_status = 0
-            while operation_status == 0:
-                user_input_hh = int(input(f"Please enter the hour(hh): "))
-                user_input_mm = int(input(f"Please enter the minutes(mm): "))
-                if (user_input_hh <= 23) and (user_input_hh >= 00) and (user_input_mm) < 60 and (user_input_mm) >= 00:
-                    operation_status = 1
-                else:
-                    print(greeting_messages.time_overflow)
-            user_input_timedelta_formatted = datetime.timedelta(hours=user_input_hh, minutes=user_input_mm)
-            return user_input_timedelta_formatted
-        except:
-            print(f"Please input appropriate values.")
+    def convert_to_timedelta(method=0, input_hours=00, input_minutes=00):
+        if method == 0:
+            try:
+                operation_status = 0
+                while operation_status == 0:
+                    user_input_hh = int(input(f"Please enter the hour(hh): "))
+                    user_input_mm = int(input(f"Please enter the minutes(mm): "))
+                    if (user_input_hh <= 23) and (user_input_hh >= 00) and (user_input_mm) < 60 and (
+                            user_input_mm) >= 00:
+                        operation_status = 1
+                    else:
+                        print(greeting_messages.time_overflow)
+                user_input_timedelta_formatted = datetime.timedelta(hours=user_input_hh, minutes=user_input_mm)
+                return user_input_timedelta_formatted
+            except:
+                print(f"Please input appropriate values.")
+        else:
+            time_delta = datetime.timedelta(hours=input_hours, minutes=input_minutes)
+            return time_delta
+
 
 """
 a = Format_time.convert_to_timedelta()
@@ -49,4 +59,3 @@ time_difference = b-a
 if time_difference >= datetime.timedelta(hours=4):
     print("success")
 """
-
